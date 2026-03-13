acf71e3ce458d641e5798cf43efec49e75842479
kokorocks <175902687+kokorocks@users.noreply.github.com>
2026-03-13 14:37:15 -0400

    idk

---
+++ public/scripts/popups.js
@@ -0,0 +1,3 @@
+function passkeyPopup(){
+    createWidget('<div class="passkey-popup"><h2>Enter Passkey</h2><input type="password" placeholder="Passkey"><button>Submit</button></div>', '0px', '0px', '300px', '150px', '200px', '100px', x=document.getElementById('mainScreen').offsetWidth/2, y=0, resizable=false, draggable=false);
+}
